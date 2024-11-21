a9153ad0cdf54b045537f95ff806e6588a5d2f2f
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2024-11-21 17:59:32 +0100

    update code formatting

--- src/pointtree/instance_segmentation/_multi_stage_algorithm.py
+++ src/pointtree/instance_segmentation/_multi_stage_algorithm.py
@@ -450,9 +450,9 @@ class MultiStageAlgorithm(InstanceSegmentationAlgorithm):  # pylint: disable=too
         height_map = np.zeros(num_cells)
         height_map[unique_grid_indices_np[:, 0], unique_grid_indices_np[:, 1]] = max_height.cpu().numpy()
         point_counts = np.zeros(num_cells)
-        point_counts[
-            unique_grid_indices_np[:, 0], unique_grid_indices_np[:, 1]
-        ] = point_counts_per_grid_cell.cpu().numpy()
+        point_counts[unique_grid_indices_np[:, 0], unique_grid_indices_np[:, 1]] = (
+            point_counts_per_grid_cell.cpu().numpy()
+        )
 
         return height_map, point_counts, first_cell * grid_size
 
@@ -883,14 +883,14 @@ class MultiStageAlgorithm(InstanceSegmentationAlgorithm):  # pylint: disable=too
                     for instance_id in neighbor_instance_ids:
                         tree_position = tree_positions_grid[instance_id - 1]
                         voronoi_id = voronoi_labels_without_border[tree_position[0], tree_position[1]]
-                        voronoi_labels_without_border_remapped[
-                            voronoi_labels_without_border == voronoi_id
-                        ] = instance_id
+                        voronoi_labels_without_border_remapped[voronoi_labels_without_border == voronoi_id] = (
+                            instance_id
+                        )
                         voronoi_labels_with_border_remapped[voronoi_labels_with_border == voronoi_id] = instance_id
 
-                    watershed_labels_without_border[
-                        neighborhood_mask_without_border
-                    ] = voronoi_labels_without_border_remapped[neighborhood_mask_without_border]
+                    watershed_labels_without_border[neighborhood_mask_without_border] = (
+                        voronoi_labels_without_border_remapped[neighborhood_mask_without_border]
+                    )
 
                     # find outer boundaries to other trees that were not included in the Voronoi segmentation
                     outer_boundaries = find_boundaries(neighborhood_mask_without_border, mode="inner", background=0)

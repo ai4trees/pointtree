0c66f62cde2f4e847a536dce047641576e14619c
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2024-12-14 20:36:00 +0100

    refactor: remove pointtorch.operations.remap_instance_ids

--- src/pointtree/instance_segmentation/__init__.py
+++ src/pointtree/instance_segmentation/__init__.py
@@ -2,7 +2,6 @@
 
 from ._instance_segmentation_algorithm import *
 from ._multi_stage_algorithm import *
-from ._utils import *
 from ._priority_queue import *
 
 __all__ = [name for name in globals().keys() if not name.startswith("_")]

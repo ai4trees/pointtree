c3854dd7ea046e21988736da300b728f7985d4b9
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2024-12-14 18:06:32 +0100

    fix: call of voxel_downsampling method

--- src/pointtree/instance_segmentation/_instance_segmentation_algorithm.py
+++ src/pointtree/instance_segmentation/_instance_segmentation_algorithm.py
@@ -113,7 +113,7 @@ class InstanceSegmentationAlgorithm(abc.ABC):
             if self._downsampling_voxel_size is not None:
                 with Timer("Voxel-based downsampling", self._time_tracker):
                     self._logger.info("Downsample point cloud...")
-                    _, predicted_point_indices, _, _ = voxel_downsampling(
+                    _, predicted_point_indices, _ = voxel_downsampling(
                         tree_points.to_numpy(), self._downsampling_voxel_size, point_aggregation="nearest_neighbor"
                     )
                     downsampled_tree_points = tree_points.iloc[predicted_point_indices]

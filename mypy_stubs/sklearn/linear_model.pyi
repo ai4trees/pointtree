3be286dde53d7f92e3d13dc9c610a2cab1ef02e7
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2024-12-15 19:22:35 +0100

    feat: estimate with linear model

---
+++ mypy_stubs/sklearn/linear_model.pyi
@@ -0,0 +1,18 @@
+__all__ = ["LinearRegression"]
+
+from typing import Optional, Union
+
+import numpy.typing as npt
+
+class LinearRegression:
+    def __init__(self, *, fit_intercept: bool = ..., copy_X: bool = ..., n_jobs: Optional[int] = ..., positive=bool):
+        self.coef_: npt.NDArray
+        self.rank_: int
+        self.singular_: npt.NDArray
+        self.intercept_: Union[float, npt.NDArray]
+        self.n_features_in_: int
+        self.feature_names_in_: npt.NDArray
+    def fit(
+        self, X: npt.ArrayLike, y: npt.ArrayLike, sample_weight: Optional[npt.ArrayLike] = None
+    ) -> LinearRegression: ...
+    def predict(self, X: npt.ArrayLike) -> npt.NDArray: ...

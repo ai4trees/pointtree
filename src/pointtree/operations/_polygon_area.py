989d7d8b773ec45e1ab95388864b4c91f3a263d7
Josafat-Mattias Burmeister <33292321+josafatburmeister@users.noreply.github.com>
2024-12-15 19:05:09 +0100

    feat: operation to compute polygon area (#88)

---
+++ src/pointtree/operations/_polygon_area.py
@@ -0,0 +1,31 @@
+""" Computation of a polygon's area. """
+
+__all__ = ["polygon_area"]
+
+import numpy as np
+import numpy.typing as npt
+
+
+def polygon_area(x: npt.NDArray[np.float64], y: npt.NDArray[np.float64]) -> float:
+    r"""
+    Computes the area of a convex polygon using the
+    `Shoelace formula <https://en.wikipedia.org/wiki/Shoelace_formula>`__. It is expected that the input vertices are
+    already sorted so that adjacent vertices are neighbors in the input vertex array.
+
+    Args:
+        x: X-coordinates of the polygon vertices.
+        y: Y-coordinates of the polygon vertices.
+
+    Returns:
+        Polygon area.
+
+    Shape:
+        - :code:`x`: :math:`(N)`
+        - :code:`y`: :math:`(N)`
+
+        | where
+        |
+        | :math:`n = \text{ number of points}`
+    """
+
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))

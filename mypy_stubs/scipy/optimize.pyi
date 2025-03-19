36d2ad726bdff3941dc48320e8abf1a791963d5d
Josafat-Mattias Burmeister <33292321+josafatburmeister@users.noreply.github.com>
2025-03-19 17:49:04 +0100

    refactor: remove old scipy stubs (#101)

--- mypy_stubs/scipy/optimize.pyi
+++
@@ -1,7 +0,0 @@
-__all__ = ["linear_sum_assignment"]
-
-from typing import Tuple
-
-import numpy as np
-
-def linear_sum_assignment(cost_matrix: np.ndarray, maximize: bool = ...) -> Tuple[np.ndarray, np.ndarray]: ...

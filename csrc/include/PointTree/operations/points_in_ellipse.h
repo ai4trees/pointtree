807ddf4ee2c68089b303d2a6aae9179a559b1264
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-05-24 10:45:59 +0200

    refactor points in ellipse function

--- csrc/include/PointTree/operations/points_in_ellipse.h
+++ csrc/include/PointTree/operations/points_in_ellipse.h
@@ -21,10 +21,8 @@ ArrayXb points_in_ellipse(RefArrayX2<scalar_T> xy, RefArrayX<scalar_T> ellipse) 
   scalar_T sin_theta = sin(theta);
 
   ArrayX<scalar_T> a = (cos_theta * (xy(Eigen::all, 0) - center_x) + sin_theta * (xy(Eigen::all, 1) - center_y));
-  a = a * a;
-  ArrayX<scalar_T> b = (sin_theta * (xy(Eigen::all, 0) - center_x) - cos_theta * (xy(Eigen::all, 1) - center_y));
-  b = b * b;
-  ArrayX<scalar_T> ellipse_equation = (a / (radius_major * radius_major)) + (b / (radius_minor * radius_minor));
+  ArrayX<scalar_T> b = (-sin_theta * (xy(Eigen::all, 0) - center_x) + cos_theta * (xy(Eigen::all, 1) - center_y));
+  ArrayX<scalar_T> ellipse_equation = (a * a / (radius_major * radius_major)) + (b * b / (radius_minor * radius_minor));
 
   return ellipse_equation <= 1;
 }
--- test/operations/test_points_in_ellipse.py
+++ test/operations/test_points_in_ellipse.py
@@ -11,9 +11,26 @@ class TestPointsInEllipse:
 
     @pytest.mark.parametrize("storage_format", ["C", "F"])
     @pytest.mark.parametrize("scalar_type", [np.float32, np.float64])
-    def test_points_in_ellipse(self, storage_format: str, scalar_type: np.dtype):
-        ellipse = np.array([1, 1, np.sqrt(18), np.sqrt(8), np.pi / 4], scalar_type, order=storage_format)
-        xy = np.array([[1, 1], [3.9, 4], [4.1, 4]], dtype=scalar_type, order=storage_format)
+    def test_points_in_diagonal_ellipse(self, storage_format: str, scalar_type: np.dtype):
+        ellipse = np.array([1, 1, 4, 2, np.pi / 4], scalar_type, order=storage_format)
+        xy = np.array(
+            [[1, 1], [0.9 + 4 / np.sqrt(2), 0.9 + 4 / np.sqrt(2)], [2.1, 1.1], [2.1, -1.1], [4.1, 4]],
+            dtype=scalar_type,
+            order=storage_format,
+        )
+
+        in_ellipse = points_in_ellipse(xy, ellipse)
+        expected_in_ellipse = np.array([True, True, True, False, False], dtype=scalar_type)
+
+        assert in_ellipse.flags.owndata is False
+
+        np.testing.assert_array_equal(expected_in_ellipse, in_ellipse)
+
+    @pytest.mark.parametrize("storage_format", ["C", "F"])
+    @pytest.mark.parametrize("scalar_type", [np.float32, np.float64])
+    def test_points_in_vertical_ellipse(self, storage_format: str, scalar_type: np.dtype):
+        ellipse = np.array([1, 1, 4, 2, np.pi / 2], scalar_type, order=storage_format)
+        xy = np.array([[1, 1], [1, 4], [4, 1]], dtype=scalar_type, order=storage_format)
 
         in_ellipse = points_in_ellipse(xy, ellipse)
         expected_in_ellipse = np.array([True, True, False], dtype=scalar_type)

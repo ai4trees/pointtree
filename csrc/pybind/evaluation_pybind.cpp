ac14ddcc912b1e01a527b2c16d03bace62455b91
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-06-23 10:18:29 +0200

    add pybind file

---
+++ csrc/pybind/evaluation_pybind.cpp
@@ -0,0 +1,29 @@
+#include <pybind11/eigen.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "../include/PointTree/evaluation/instance_segmentation_metrics.h"
+
+PYBIND11_MODULE(_evaluation_cpp, m) {
+  m.doc() = R"pbdoc(
+    C++ extension module implementing evaluation functions.
+  )pbdoc";
+
+  m.def(
+      "compute_instance_segmentation_metrics_per_partition",
+      &PointTree::compute_instance_segmentation_metrics_per_partition<float>,
+      pybind11::return_value_policy::reference_internal, "");
+
+  m.def(
+      "compute_instance_segmentation_metrics_per_partition",
+      &PointTree::compute_instance_segmentation_metrics_per_partition<double>,
+      pybind11::return_value_policy::reference_internal,
+      R"pbdoc(Calculates instance segmentation metrics for different spatial partitions of a tree instance.)pbdoc");
+
+#ifdef VERSION_INFO
+  m.attr("__version__") = (VERSION_INFO);
+#else
+  m.attr("__version__") = "dev";
+#endif
+}

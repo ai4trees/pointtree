3f1a50b6e5784011f7cda6285bbe91da5d33dd2b
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-06-23 11:25:37 +0200

    update code for instance segmentation metrics per partition

--- csrc/include/PointTree/evaluation/instance_segmentation_metrics.h
+++ csrc/include/PointTree/evaluation/instance_segmentation_metrics.h
@@ -132,14 +132,7 @@ std::tuple<ArrayXX<scalar_T>, ArrayXX<scalar_T>, ArrayXX<scalar_T>> compute_inst
   ArrayXX<scalar_T> recall =
       ArrayXX<scalar_T>::Constant(num_target_ids, num_partitions, std::numeric_limits<scalar_T>::quiet_NaN());
 
-  ArrayXX<scalar_T> intersection_count = ArrayXX<scalar_T>::Constant(num_target_ids, num_partitions, 0);
-  ArrayXX<scalar_T> prediction_count = ArrayXX<scalar_T>::Constant(num_target_ids, num_partitions, 0);
-  ArrayXX<scalar_T> target_count = ArrayXX<scalar_T>::Constant(num_target_ids, num_partitions, 0);
-
-  scalar_T partition_size = 1.0 / num_partitions;
-
-  MatrixX2<scalar_T> xy = xyz(Eigen::all, {0, 1}).matrix();
-  KDTree2<scalar_T>* kd_tree = new KDTree2<scalar_T>(2, std::cref(xy), 10 /* max leaf size */);
+  VectorXd intervals = VectorXd::LinSpaced(num_partitions + 1, 0.0, 1.0);
 
 #pragma omp parallel for num_threads(num_workers)
   for (int64_t target_idx = 0; target_idx < num_target_ids; ++target_idx) {
@@ -158,11 +151,9 @@ std::tuple<ArrayXX<scalar_T>, ArrayXX<scalar_T>, ArrayXX<scalar_T>> compute_inst
     }
 
     ArrayX3<scalar_T> tree_xyz = xyz(target_indices, Eigen::all);
+
     scalar_T min_z = tree_xyz.col(2).minCoeff();
-
-    ArrayX<scalar_T> distance;  //  = ArrayX<scalar_T>::Zero(num_points);
-
-    std::vector<nanoflann::ResultItem<int64_t, scalar_T>> search_result;
+    ArrayX<scalar_T> distance;
 
     if (partition == "xy") {
       scalar_T z_threshold = min_z + 0.30;
@@ -175,88 +166,59 @@ std::tuple<ArrayXX<scalar_T>, ArrayXX<scalar_T>, ArrayXX<scalar_T>> compute_inst
 
       Eigen::Array<scalar_T, 1, 2> position = tree_xyz(lowest_point_indices, {0, 1}).colwise().mean();
 
-      ArrayX2<scalar_T> tree_xy_centered = tree_xyz.leftCols(2).rowwise() - position;
-      ArrayX<scalar_T> distances_target = tree_xy_centered.rowwise().norm();
+      ArrayX<scalar_T> distance_target = (tree_xyz.leftCols(2).rowwise() - position).rowwise().norm();
 
-      scalar_T regularized_max = static_cast<scalar_T>(quantile(distances_target, 0.95));
-      scalar_T regularized_max_squared = regularized_max * regularized_max;
+      scalar_T regularized_max = static_cast<scalar_T>(quantile(distance_target, 0.95));
 
-      const size_t num_neighbors =
-          kd_tree->index_->radiusSearch(position.data(), regularized_max_squared, search_result);
-
-      std::vector<int64_t> neighbor_indices;
-      distance = ArrayX<scalar_T>(num_neighbors);
-
-      for (int64_t i = 0; i < num_neighbors; ++i) {
-        neighbor_indices.push_back(search_result[i].first);
-      }
-      distance = (xy(neighbor_indices, Eigen::all).array().rowwise() - position).rowwise().norm() / regularized_max;
+      distance = (xyz.leftCols(2).rowwise() - position).rowwise().norm();
+      distance /= regularized_max;
 
     } else if (partition == "z") {
-      Eigen::Array<scalar_T, 1, 2> position = tree_xyz(Eigen::all, {0, 1}).colwise().mean();
       ArrayX<scalar_T> z_values = tree_xyz.col(2) - min_z;
       scalar_T regularized_max = static_cast<scalar_T>(quantile(z_values, 0.95));
-
-      std::vector<int64_t> prediction_indices;
-      for (int64_t i = 0; i < num_points; ++i) {
-        if (prediction(i) == predicted_id) {
-          prediction_indices.push_back(i);
-        }
-      }
-      ArrayX2<scalar_T> prediction_xy = xyz(prediction_indices, {0, 1});
-
-      scalar_T radius = (tree_xyz.leftCols(2).rowwise() - position).rowwise().norm().maxCoeff();
-      radius = std::max(radius, (prediction_xy.rowwise() - position).rowwise().norm().maxCoeff()) + 1.0;
-      scalar_T radius_squared = radius * radius;
-
-      const size_t num_neighbors = kd_tree->index_->radiusSearch(position.data(), radius_squared, search_result);
-
-      distance = ArrayX<scalar_T>(num_neighbors);
-
-      for (int64_t i = 0; i < num_neighbors; ++i) {
-        distance(i) = (xyz(search_result[i].first, 2) - min_z) / regularized_max;
-      }
+      distance = (xyz.col(2) - min_z) / (regularized_max);
     }
 
-    for (int64_t i = 0; i < search_result.size(); ++i) {
-      int64_t idx = search_result[i].first;
+    ArrayX<scalar_T> intersection_count = ArrayX<scalar_T>::Zero(num_partitions);
+    ArrayX<scalar_T> target_count = ArrayX<scalar_T>::Zero(num_partitions);
+    ArrayX<scalar_T> prediction_count = ArrayX<scalar_T>::Zero(num_partitions);
+    scalar_T partition_size = 1.0 / num_partitions;
+
+    for (int64_t i = 0; i < num_points; ++i) {
       int64_t partition_idx = static_cast<int64_t>(std::floor(distance(i) / partition_size));
 
       if (partition_idx < 0 || partition_idx >= num_partitions) {
         continue;
       }
 
-      if (target(idx) == target_id) {
-        target_count(target_idx, partition_idx) += 1.0;
+      if (target(i) == target_id) {
+        target_count(partition_idx) += 1;
       }
-
       if (predicted_id != invalid_instance_id) {
-        if (target(idx) == target_id && prediction(idx) == predicted_id) {
-          intersection_count(target_idx, partition_idx) += 1.0;
+        if (prediction(i) == predicted_id) {
+          prediction_count(partition_idx) += 1;
         }
-
-        if (prediction(idx) == predicted_id) {
-          prediction_count(target_idx, partition_idx) += 1.0;
+        if (target(i) == target_id && prediction(i) == predicted_id) {
+          intersection_count(partition_idx) += 1;
         }
       }
     }
 
+    ArrayX<scalar_T> union_count = target_count + prediction_count - intersection_count;
+
     for (int64_t i = 0; i < num_partitions; ++i) {
-      scalar_T union_count =
-          target_count(target_idx, i) + prediction_count(target_idx, i) - intersection_count(target_idx, i);
-      if (union_count > 0) {
-        iou(target_idx, i) = intersection_count(target_idx, i) / union_count;
+      if (union_count(i) > 0) {
+        iou(target_idx, i) = intersection_count(i) / union_count(i);
       }
-      if (prediction_count(target_idx, i) > 0) {
-        precision(target_idx, i) = intersection_count(target_idx, i) / prediction_count(target_idx, i);
+      if (target_count(i) > 0) {
+        recall(target_idx, i) = intersection_count(i) / target_count(i);
       }
-      if (target_count(target_idx, i) > 0) {
-        recall(target_idx, i) = intersection_count(target_idx, i) / target_count(target_idx, i);
+      if (prediction_count(i) > 0) {
+        precision(target_idx, i) = intersection_count(i) / prediction_count(i);
       }
     }
   }
 
-  delete kd_tree;
   return {iou, precision, recall};
 }
 

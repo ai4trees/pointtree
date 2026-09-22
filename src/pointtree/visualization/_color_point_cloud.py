8da8fec226145dea7c20d3c24308672fc0f19dda
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2026-09-22 15:02:29 +0200

    fix CI issues

--- src/pointtree/visualization/_color_point_cloud.py
+++ src/pointtree/visualization/_color_point_cloud.py
@@ -2,7 +2,6 @@
 
 __all__ = ["color_semantic_segmentation", "color_instance_segmentation"]
 
-import math
 from typing import Dict, List, Optional
 
 import numpy as np
@@ -12,7 +11,7 @@ from ._color_palette import color_palette, acm_red, acm_blue
 from ._hex_to_rgb import hex_to_rgb
 
 
-def color_instance_segmentation(
+def color_instance_segmentation(  # pylint: disable=too-many-locals
     point_cloud: pd.DataFrame,
     instance_id_column: str,
     target_instance_id_column: Optional[str] = None,
@@ -47,21 +46,21 @@ def color_instance_segmentation(
     instance_ids = instance_ids[instance_ids >= 0]
 
     if shift is not None:
-        color_palette_shifted = np.roll(color_palette, shift, axis=0)
+        color_palette_shifted = np.roll(color_palette, shift, axis=0).tolist()
     else:
         color_palette_shifted = color_palette
 
     random_generator = np.random.default_rng(seed=seed)
 
-    colors = []
+    colors: List[List[int]] = []
     shift = 0
     while len(colors) < len(instance_ids):
         colors.extend(np.roll(color_palette_shifted, shift, axis=0))
-        shift += random_generator.integers(1, len(color_palette))
+        shift += int(random_generator.integers(1, len(color_palette)))
 
     indices = [185, 221]
     for i in indices:
-        if i > len(colors) -1:
+        if i > len(colors) - 1:
             continue
         random_generator.choice(colors, axis=0)
         colors[i] = random_generator.choice(colors, axis=0)
@@ -74,7 +73,7 @@ def color_instance_segmentation(
 
     for instance_id in instance_ids:
         point_cloud.loc[
-            point_cloud[instance_id_column] == instance_id,
+            point_cloud[instance_id_column] == instance_id,  # type: ignore[index]
             ["r", "g", "b", "a"],
         ] = colors[color_idx]
 
